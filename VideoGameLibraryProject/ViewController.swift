237b23d4ee2a45f0bfe7304ab594d5da38bf8113
Jamie Cummings <jamiecummings@MacBook-Pro-2.local>
2018-10-15 12:41:07 -0500

    Initial Commit

---
+++ VideoGameLibraryProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoGameLibraryProject
+//
+//  Created by Jamie Cummings on 10/15/18.
+//  Copyright © 2018 Jamie Cummings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
